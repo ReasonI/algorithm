32e15b090e319fa2ce5d05200ed661866d6130eb
ReasonI <lemonlee030@gmail.com>
2021-06-05 14:39:39 +0900

    [백준] 2252 '줄 세우기'

--- java/test/src/Main_2252.java
+++ java/test/src/Main_2252.java
@@ -1,54 +1,61 @@
+import java.io.*;
 import java.util.*;
 
-public class Main_11729 {
+public class Main_2252 {
 
     static int N;
     static int M;
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        N = scanner.nextInt();
-        M = scanner.nextInt();
+    public static void main(String[] args) throws Exception {
 
-        int[] edgesNum = new int[N + 1];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        int[] edgesNum = new int[N + 1]; //edgesNum 배열
 
         ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-        for (int i = 0; i < N + 1; i++) {
+        for(int i = 0; i < N + 1; i++){ //그래프
             graph.add(new ArrayList<Integer>());
         }
 
-        for (int i = 0; i < M; i++) {
-            int v1 = scanner.nextInt();
-            int v2 = scanner.nextInt();
-            graph.get(v1).add(v2);
-            edgesNum[v2]++;
-        }
+        for(int i = 0; i < M ; i++){ //그래프 초기화
+            StringTokenizer st2 = new StringTokenizer(br.readLine());
 
-        topologicalSort(graph, edgesNum);
+            int vertex1 = Integer.parseInt(st2.nextToken());
+            int vertex2 = Integer.parseInt(st2.nextToken());
+
+            graph.get(vertex1).add(vertex2);
+            edgesNum[vertex2]++;
+        }
+        topologicalSort(edgesNum, graph);
+
     }
 
-    static void topologicalSort(ArrayList<ArrayList<Integer>> graph, int[] edgesNum) {
-        Queue<Integer> queue = new LinkedList();
+    static void topologicalSort(int[] edgesNum, ArrayList<ArrayList<Integer>> graph){
 
-        for (int i = 1; i < N + 1; i++) {
-            if (edgesNum[i] == 0) {
-                queue.add(i);
+        Queue<Integer> zeroIndegree = new LinkedList<>();
+
+        for(int i = 1; i < N + 1; i++){ // 먼저 indegree가 0인 vertex 먼저 넣
+            if(edgesNum[i] == 0){
+                zeroIndegree.add(i);
             }
         }
 
-        for (int i = 0; i < N; i++) {
-            int v = queue.remove();
-            System.out.print(v + " ");
+        for(int i = 0; i < N; i++){
+            int vertex = zeroIndegree.remove();
+            System.out.print(vertex + " ");
 
-
-            for (int nextV : graph.get(v)) {
+            for(int nextV : graph.get(vertex)){
                 edgesNum[nextV]--;
 
-                if (edgesNum[nextV] == 0) {
-                    queue.add(nextV);
+                if(edgesNum[nextV] == 0){
+                    zeroIndegree.add(nextV);
                 }
             }
         }
-    }
 
+    }
 }
