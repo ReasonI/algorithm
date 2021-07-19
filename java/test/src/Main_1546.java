385f75860db5cf5d306bcdb8e8fe4c8c62ea117f
ReasonI <lemonlee030@gmail.com>
2021-07-20 00:21:08 +0900

    [백준] 1546 '평균'

--- java/test/src/Main_1546.java
+++ java/test/src/Main_1546.java
@@ -6,30 +6,24 @@ public class Main_1546 {
         Scanner sn = new Scanner(System.in);
 
         int num = sn.nextInt();
-        int max = 0;
-        int count = 0;
         float answer = 0;
-        int[] number = new int[num];
+        float[] number = new float[num];
 
-        for(int i = 0; i < num; i++){
+        number[0] = sn.nextInt();
+        float max = number[0];
+
+        for(int i = 1; i < num; i++){
             number[i] = sn.nextInt();
-        }
 
-        for(int i = 0; i < num; i++){
-            if (max < number[i]){
+            if(max < number[i]){
                 max = number[i];
-                count = i;
             }
         }
 
         for(int i = 0;i < num; i++){
-            if(i == count){
-                continue;
-            }else{
-                answer = answer + number[i] / max * 100;
-            }
+                answer += (number[i] / max) * 100;
         }
 
-            System.out.print(answer/num);
+            System.out.print(answer / num);
     }
 }
