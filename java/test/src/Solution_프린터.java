e05e98c345510ee287d3365af34ec4a0933f0d03
ReasonI <lemonlee030@gmail.com>
2021-07-23 23:35:55 +0900

    [프골그래머스] 스택/큐 '프린터'

---
+++ java/test/src/Solution_프린터.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution_프린터 {
+    public int solution(int[] priorities, int location) {
+        int answer = 1;
+
+        PriorityQueue priority = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int task : priorities){
+            priority.add(task);
+            System.out.println(priority);
+        } System.out.println(priority);
+
+        while(!priority.isEmpty()){
+            for(int i=0; i<priorities.length; i++){
+                if(priorities[i] == (int)priority.peek()) {
+                    if(i == location){ return answer; }
+
+                    priority.poll();
+                    answer++;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
