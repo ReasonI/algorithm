3e14a63813951d427d7ba29ef889ef99725e9b0e
ReasonI <lemonlee030@gmail.com>
2021-07-30 13:41:05 +0900

    해시로 풀어보기

--- java/test/src/Solution_완주하지_못한_선수.java
+++ java/test/src/Solution_완주하지_못한_선수.java
@@ -1,17 +1,27 @@
-
-import java.util.*;
+import java.util.HashMap;
 
 class Solution_완주하지_못한_선수 {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        Arrays.sort(participant);
-        Arrays.sort(completion);
 
-        for(int i=0; i<completion.length; i++){
-            if(!participant[i].equals(completion[i]))
-                return participant[i];
+        String answer = "";
+
+        HashMap<String, Integer> hm = new HashMap<>();
+
+        for (String player : participant) {
+            hm.put(player, hm.getOrDefault(player, 0) + 1);
         }
 
-        return participant[participant.length-1];
+        for (String player : completion) {
+            hm.put(player, hm.get(player) - 1);
+        }
+
+        for (String key : hm.keySet()) {
+            if (hm.get(key) != 0) {
+                answer = key;
+                break;
+            }
+        }
+
+        return answer;
     }
 }
