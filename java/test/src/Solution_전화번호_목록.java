ab1e5c77b42f400c686e9eea7989dc80fd0b7f07
ReasonI <lemonlee030@gmail.com>
2021-07-31 23:58:45 +0900

    [프로그래머스] 해시 '전화번호 목록'

---
+++ java/test/src/Solution_전화번호_목록.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+class Solution_전화번호_목록 {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        return answer;
+
+        HashMap<String,Integer> hm = new HashMap<>();
+
+        for(int i = 0; i < phone_book.length; i++){
+            hm.put(phone_book[i], i);
+        }
+
+        for(int i = 0; i < phone_book.length; i++){
+            for(int j = 1; j < phone_book[i].length; j++){
+                if(hm.containsKey(phone_book[i].substring(0,j))){
+                    answer = false;
+                    return answer;
+                }
+
+            }
+        }
+
+        return answer;
+    }
+}
