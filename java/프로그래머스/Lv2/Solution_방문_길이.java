9af99d4e9ce7b0d0da14e305b99be76645339aae
ReasonI <lemonlee030@gmail.com>
2021-08-09 23:59:16 +0900

    [프로그래머스] '방문길이'

---
+++ java/프로그래머스/Lv2/Solution_방문_길이.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+class Solution_방문_길이 {
+    public int solution(String dirs) {
+        int answer = 0;
+        int[] dX = {0,0,-1,1};
+        int[] dY = {-1,1,0,0};
+        
+
+
+        return answer;
+    }
+}
