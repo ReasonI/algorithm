b8478988336df0ce2bbb0eb17ea6bed40da812bc
ReasonI <lemonlee030@gmail.com>
2021-08-10 23:58:12 +0900

    [프로그래머스] s/w coding '방문 길이'

--- java/프로그래머스/Lv2/Solution_방문_길이.java
+++ java/프로그래머스/Lv2/Solution_방문_길이.java
@@ -1,14 +1,47 @@
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.PriorityQueue;
-
 class Solution_방문_길이 {
     public int solution(String dirs) {
         int answer = 0;
-        int[] dX = {0,0,-1,1};
-        int[] dY = {-1,1,0,0};
-        
+        int startX = 0;
+        int startY = 0;
+        int endX = 5;
+        int endY = 5;
+        int index = 0;
 
+        int[] dX = {-1,1,0,0};
+        int[] dY = {0,0,-1,1};
+        boolean[][][][] visit = new boolean[11][11][11][11];
+
+        for(String dir : dirs){
+            startX = endX;
+            startY = endY;
+
+            switch(dir){
+                case "U" :
+                    index = 3;
+                    break;
+                case "D" :
+                    index = 2;
+                    break;
+                case "R" :
+                    index = 1;
+                    break;
+                case "L" :
+                    index = 0;
+                    break;
+                default :
+                    break;
+            }
+
+            endX = dX[index];
+            endY = dY[index];
+
+            if(endX < 0 || endY < 0 || endX > 10 || endY > 10){
+                endX = startX;
+                endY = startY;
+            }
+
+            // if(!visit[startX][startY][endX][endY] && !visit)
+        }
 
         return answer;
     }
