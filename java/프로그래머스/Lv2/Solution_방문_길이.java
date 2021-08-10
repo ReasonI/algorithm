aa97e7867d44d7f986bbc3fdac6ddba8a13c50ae
ReasonI <lemonlee030@gmail.com>
2021-08-11 00:11:20 +0900

    Fix myPageList signUp user to last

--- java/프로그래머스/Lv2/Solution_방문_길이.java
+++ java/프로그래머스/Lv2/Solution_방문_길이.java
@@ -1,4 +1,4 @@
-class Solution_방문_길이 {
+class Solution {
     public int solution(String dirs) {
         int answer = 0;
         int startX = 0;
@@ -11,36 +11,41 @@ class Solution_방문_길이 {
         int[] dY = {0,0,-1,1};
         boolean[][][][] visit = new boolean[11][11][11][11];
 
-        for(String dir : dirs){
+        for(int i = 0; i < dirs.length(); i++){
             startX = endX;
             startY = endY;
 
-            switch(dir){
-                case "U" :
+            switch(dirs.charAt(i)){
+                case 'U' :
                     index = 3;
                     break;
-                case "D" :
+                case 'D' :
                     index = 2;
                     break;
-                case "R" :
+                case 'R' :
                     index = 1;
                     break;
-                case "L" :
+                case 'L' :
                     index = 0;
                     break;
                 default :
                     break;
             }
 
-            endX = dX[index];
-            endY = dY[index];
+            endX += dX[index];
+            endY += dY[index];
 
             if(endX < 0 || endY < 0 || endX > 10 || endY > 10){
                 endX = startX;
                 endY = startY;
+                continue;
             }
 
-            // if(!visit[startX][startY][endX][endY] && !visit)
+            if(!visit[startX][startY][endX][endY] && !visit[endX][endY][startX][startY]){
+                visit[startX][startY][endX][endY] = true;
+                visit[endX][endY][startX][startY] = true;
+                answer++;
+            }
         }
 
         return answer;
