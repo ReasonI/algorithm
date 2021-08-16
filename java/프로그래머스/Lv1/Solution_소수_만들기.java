f6d7f6a49dd5c21a1cac83b31f37d79baa2f674c
ReasonI <lemonlee030@gmail.com>
2021-08-16 16:16:16 +0900

    [프로그래머스] s/w coding(2018) '소수 만들기'

--- java/프로그래머스/Lv1/Solution_소수_만들기.java
+++ java/프로그래머스/Lv1/Solution_소수_만들기.java
@@ -1,22 +1,27 @@
-import java.util.HashSet;
+class Solution_소수_만들기 {
+    public int solution(int[] nums) {
+        int answer = 0;
+        int num = 0;
+        boolean status = true;
 
-class Solution {
-    public int[] solution(int n, String[] words) {
-        int[] answer = new int[2];
-        HashSet<String> set = new HashSet<>();
+        for(int i = 0; i < nums.length - 2; i++){
+            for(int j = i + 1; j < nums.length - 1; j++){
+                for (int k = j + 1; k < nums.length; k++){
+                    num = nums[i] + nums[j] + nums[k];
+                    status = true;
+                    for(int x = 2; x < num; x++){
 
-        set.add(words[0]);
+                        if(num % x == 0){
+                            status = false;
+                            break;
+                        }
+                    }
+                    if(status == true){
+                        answer++;
+                    }
+                }
 
-        for (int i = 1; i < words.length; i++) {
-
-            if (set.contains(i) || words[i - 1].charAt(words[i - 1].length() - 1) != words[i].charAt(0)) {
-                answer[0] = i % n + 1;
-                answer[1] = i / n + 1;
-
-                break;
             }
-            set.add(words[i]);
-
         }
 
         return answer;
