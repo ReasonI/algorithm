54819c41452594fa61b5b3985f8cb4a5401d3ee8
ReasonI <lemonlee030@gmail.com>
2021-09-03 12:37:24 +0900

    [백준] 10952 'A+B - 5'

--- java/백준/Main_10952.java
+++ java/백준/Main_10952.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
 
-public class Main_10951 {
+public class Main_10952 {
     public static void main(String args[]) {
 
         Scanner sc = new Scanner(System.in);
 
-        while (sc.hasNextInt()) {
+        while (true) {
             int A = sc.nextInt();
             int B = sc.nextInt();
+
+            if(A == 0) break;
             System.out.println(A + B);
 
         }
