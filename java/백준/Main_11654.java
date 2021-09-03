d511360fa36f8bf0a8e88e94b9aedb07f9cfd70f
ReasonI <lemonlee030@gmail.com>
2021-09-03 12:56:43 +0900

    [백준] 11654 '아스키 코드'

---
+++ java/백준/Main_11654.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main_11654 {
+    public static void main(String args[]) throws IOException {
+
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.print((int) sc.next().charAt(0));
+
+        int a = System.in.read();
+        System.out.print(a);
+
+    }
+}
