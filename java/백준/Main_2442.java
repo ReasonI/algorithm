3e1eee905ec16473a4372c13bb7afc226534752f
ReasonI <lemonlee030@gmail.com>
2021-09-07 22:33:00 +0900

    [백준] 2442 '별 찍기 - 5'

--- java/백준/Main_2442.java
+++ java/백준/Main_2442.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
 
-public class Main_2441 {
+public class Main_2442 {
     public static void main(String args[]) {
 
         Scanner sc = new Scanner(System.in);
 
         int num = sc.nextInt();
 
-        for(int i = num; i > 0; i--){
-            for(int j = 0; j < num - i; j++){
+        for (int i = 1; i < num + 1; i++) {
+            for (int j = 0; j < num - i; j++) {
                 System.out.print(" ");
             }
-            for(int k = i; k > 0; k--){
+            for (int k = 0; k < i * 2 - 1; k++) {
                 System.out.print("*");
+
             }
             System.out.println("");
         }
