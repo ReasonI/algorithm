59f7a984c10327f608d39c432b1c1563caa8f0ce
ReasonI <lemonlee030@gmail.com>
2021-09-07 22:36:33 +0900

    [백준] 2443 '별 찍기 - 6'

--- java/백준/Main_2443.java
+++ java/백준/Main_2443.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
-public class Main_2442 {
+public class Main_2443 {
     public static void main(String args[]) {
 
         Scanner sc = new Scanner(System.in);
 
         int num = sc.nextInt();
 
-        for (int i = 1; i < num + 1; i++) {
+        for (int i = num; i > 0; i--) {
             for (int j = 0; j < num - i; j++) {
                 System.out.print(" ");
             }
