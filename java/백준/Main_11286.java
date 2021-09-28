3f01ca00163f8c7bc826f4d7148e8dd83363a344
ReasonI <lemonlee030@gmail.com>
2021-09-28 17:58:41 +0900

    [백준] 11286 '절댓값 힙'

--- java/백준/Main_11286.java
+++ java/백준/Main_11286.java
@@ -1,14 +1,21 @@
 import java.io.*;
-import java.util.Collections;
 import java.util.PriorityQueue;
 
-public class Main_11279 {
+public class Main_11286 {
     public static void main(String args[]) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int tc = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) ->{
+            int abs1 = Math.abs(a);
+            int abs2 = Math.abs(b);
+
+            if(abs1 == abs2) return a > b ? 1 : -1;
+
+            return abs1 - abs2;
+
+        });
 
         for (int i = 0; i < tc; i++) {
 
