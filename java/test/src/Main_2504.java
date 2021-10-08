ae1fd80da7836fc46af769f3d515141ffb94aafc
ReasonI <lemonlee030@gmail.com>
2021-10-08 21:40:47 +0900

    [백준] 2504 '괄호의 값'

--- java/test/src/Main_2504.java
+++ java/test/src/Main_2504.java
@@ -10,32 +10,70 @@ public class Main_2504 {
         String word = br.readLine().toUpperCase();
 
         Stack<String> stack = new Stack<>();
+        Integer num = 0;
 
-        stack.add(word.substring(0,1));
+        for (int i = 0; i < word.length(); i++) {
 
-        for (int i = 1; i < word.length(); i++) {
+            String bracket = word.substring(i, i + 1);
 
-            String bracket = word.substring(i,i+1);
-
-            if (bracket == "(" || bracket == "[") {
+            if (bracket.equals("(") || bracket.equals("[")) {
                 stack.add(bracket);
                 continue;
             }
 
+            if (stack.isEmpty()) {
+                System.out.print(0);
+                return;
+            }
             String temp = stack.pop();
 
-            if(temp == "(" && bracket == ")") stack.add("2");
-            else if(temp == "[" && bracket == "]") stack.add("3");
-            else if(temp != "(" || temp != "["){
-                stack.pop();
-                if( bracket == ")") stack.add(String.valueOf(Integer.parseInt(temp) * 2));
-                else stack.add(String.valueOf(Integer.parseInt(temp) * 3));
-            }else{
+            if (temp.equals("(") && bracket.equals(")")) stack.add("2");
+            else if (temp.equals("[") && bracket.equals("]")) stack.add("3");
+            else if (temp.equals("(") || temp.equals("[")) {  // 숫자가 아닐 경우
+                System.out.print(0);
+                return;
+            } else { //숫자일 경우
+                if (stack.isEmpty()) {
+                    System.out.print(0);
+                    return;
+                }
+                while (!stack.peek().equals("(") && !stack.peek().equals("[")) { // 숫자
+                    temp = String.valueOf(Integer.parseInt(stack.pop()) + Integer.parseInt(temp));
+                    if (stack.isEmpty()) {
+                        System.out.print(0);
+                        return;
+                    }
+                }
+                if (stack.isEmpty()) {
+                    System.out.print(0);
+                    return;
+                }
+                if (!stack.peek().equals("(") && bracket.equals(")")) {
+                    System.out.print(0);
+                    return;
+                } else if (!stack.peek().equals("[") && bracket.equals("]")) {
+                    System.out.print(0);
+                    return;
+                }
 
+                stack.pop();
+
+                if (bracket.equals(")")) stack.add(String.valueOf(Integer.parseInt(temp) * 2));
+                else stack.add(String.valueOf(Integer.parseInt(temp) * 3));
             }
 
-
         }
+
+        while (!stack.isEmpty()) {
+            if (stack.peek().equals("(") || stack.peek().equals("[")) {  // 숫자가 아닐 경우
+                System.out.print(0);
+                return;
+            } else {
+                num += Integer.parseInt(stack.pop());
+            }
+        }
+
+        System.out.print(num);
 
         br.close();
 
