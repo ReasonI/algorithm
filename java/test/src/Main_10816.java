eeb15539b3b3d92f7d1c86ed75fefdd0e1e14885
ReasonI <lemonlee030@gmail.com>
2021-12-07 23:52:59 +0900

    [백준] 10816 '숫자 카드2'

---
+++ java/test/src/Main_10816.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main_10816 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int[] NArray = new int[N];
+
+        for(int i = 0; i < N; i++){
+            NArray[i] = sc.nextInt();
+        }
+
+        Arrays.sort(NArray);
+
+        int M = sc.nextInt();
+
+        for(int i = 0; i < M; i++){
+
+        }
+
+    }
+
+
+
+}
